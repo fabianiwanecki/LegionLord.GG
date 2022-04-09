cb366e3ac2038dfdab1a34b4808a50038951f4c3
Fabian Iwanecki <fabian@iwanecki.de>
2022-04-09 21:35:10 +0200

    Refactor snackbar component

--- src/app/shared/player-not-found-snack/player-not-found-snack.component.ts
+++ src/app/shared/player-not-found-snack/player-not-found-snack.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-player-not-found-snack',
   templateUrl: './player-not-found-snack.component.html',
   styleUrls: ['./player-not-found-snack.component.scss']
 })
-export class PlayerNotFoundSnackComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class PlayerNotFoundSnackComponent {
 
 }
