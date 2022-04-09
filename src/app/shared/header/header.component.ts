809154d3f5e10cb9d07b025e54d06db1c7dd62af
Fabian Iwanecki <fabian@iwanecki.de>
2022-04-09 21:34:21 +0200

    Refactor header component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,8 +1,4 @@
 import {Component} from '@angular/core';
-import {FormBuilder} from "@angular/forms";
-import {Router} from "@angular/router";
-import {PlayerService} from "../services/player.service";
-import {SnackbarService} from "../services/snackbar.service";
 
 @Component({
   selector: 'app-header',
@@ -10,41 +6,4 @@ import {SnackbarService} from "../services/snackbar.service";
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
-
-  formSearchPlayer = this.fb.group({
-    playerName: [],
-  })
-  loading: boolean = false;
-  notFound: boolean = false;
-
-  constructor(private fb: FormBuilder,
-              private router: Router,
-              private snackbarService: SnackbarService,
-              private playerService: PlayerService) {
-  }
-
-  submit() {
-    if (this.formSearchPlayer.controls['playerName'].value) {
-      this.loading = true;
-      this.playerService.searchPlayerByName(this.formSearchPlayer.controls['playerName'].value).subscribe({
-        next: (player: any) => {
-          this.router.navigate(['/player', player['_id']]);
-          this.loading = false;
-          this.notFound = false;
-        },
-        error: () => {
-          this.loading = false
-          this.notFound = true;
-          this.snackbarService.openSnackBarPlayerNotFound();
-        },
-      });
-    }
-  }
-
-  onKey(event: KeyboardEvent) {
-    if (event.key === 'Enter') {
-      this.submit();
-    }
-  }
 }
